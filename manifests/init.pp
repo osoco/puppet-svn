347a3d588d5046c642ce0681809a7a5c8e117505
deigote <diego@toharia.com>
2012-06-05 10:26:57 +0200

    The svn checkout is only done if the path doesnt exists, created an update define

--- manifests/init.pp
+++ manifests/init.pp
@@ -18,6 +18,24 @@ define svn::checkout($username,$password,$url,$path,$trust-cert='true',$timeout=
 
     exec {"svn-co-$name":
         command => "/usr/bin/svn --username $username --password $password --non-interactive $trust-server-cert-option checkout $url $path",
-        timeout => $timeout
+        timeout => $timeout,
+	creates => "$path"
     }
+}
+
+define svn::update($path, $username, $password, $trust-cert='true', $timeout=60) {
+
+    if $trust-cert == 'true' {
+        $trust-server-cert-option = "--trust-server-cert"
+    } else {
+        $trust-server-cert-option = ""
+    }
+
+    exec {"svn-up-$name":
+        command => "/usr/bin/svn --username $username --password $password --non-interactive $trust-server-cert-option up",
+        cwd => "$path"
+        timeout => $timeout,
+        onlyif => "test -d $path/.svn"
+    }
+
 }
