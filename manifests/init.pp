41d6614d6eeb4f2b7a357e4723d9c797a5a1233e
deigote <diego@toharia.com>
2012-10-25 19:13:30 +0200

    Added a refreshonly option to checkout

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,7 +8,7 @@ class svn ($ensure = 'latest', $package = 'subversion') {
 
 }
 
-define svn::checkout($username,$password,$url,$path,$trust-cert='true',$timeout=60) {
+define svn::checkout($username,$password,$url,$path,$trust-cert='true',$timeout=60, $refreshonly = false) {
 
     if $trust-cert == 'true' {
         $trust-server-cert-option = "--trust-server-cert"
@@ -19,7 +19,8 @@ define svn::checkout($username,$password,$url,$path,$trust-cert='true',$timeout=
     exec {"svn-co-$name":
         command => "/usr/bin/svn --username $username --password $password --non-interactive $trust-server-cert-option checkout $url $path",
         timeout => $timeout,
-	creates => "$path"
+	creates => "$path",
+        refreshonly => "$refreshonly"
     }
 }
 
@@ -37,5 +38,5 @@ define svn::update($path, $username, $password, $trust-cert='true', $timeout=60)
         timeout => $timeout,
         onlyif => "test -d $path/.svn"
     }
-
 }
+
